090ac13518d8258529eb65a01681a44b6a5c5757
Thodoris Kouleris <kurokokoro@windowslive.com>
2017-03-15 22:03:05 +0200

    db changes file

--- class/db.changes.php
+++ class/db.changes.php
@@ -2,3 +2,4 @@
 
 ALTER TABLE `icmail`.`users` ADD lastname VARCHAR(45) NULL;
 ALTER TABLE `icmail`.`users` ADD name VARCHAR(45) NULL;
+ALTER TABLE `icmail`.`receivedemails` ADD emaiilStatus BIT(1) NULL;
