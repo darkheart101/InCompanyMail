0221cb83cbf1bf88aae054120462e50cd5915b1c
darkheart101 <kurokokoro@windowslive.com>
2017-11-28 21:28:10 +0200

    Initial DB

--- class/db.queries.php
+++ class/db.queries.php
@@ -1,122 +1,21 @@
-<?php
-	require_once($_SERVER['DOCUMENT_ROOT']."/incompanymail/class/class.database.php");
-	require_once($_SERVER['DOCUMENT_ROOT']."/incompanymail/config.php");
-	session_start();
+CREATE TABLE `users` (
+  `idusers` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `usermail` varchar(50) NOT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusers`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 
-		$db_name = DB_NAME;
+CREATE TABLE `receivedemails` (
+  `idmail` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `fromID` int(11) NOT NULL,
+  `msg` varchar(255) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `emailStatus` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`idmail`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
-		$database = Database::getInstance();
-			
-		$query = "
-
-			CREATE TABLE  IF NOT EXISTS  `$db_name`.`users` (
-				`idusers` INT NOT NULL AUTO_INCREMENT,
-				`username` VARCHAR(45) NULL,
-				`password` VARCHAR(45) NULL,
-				PRIMARY KEY (`idusers`)
-			);
-
-			INSERT INTO users(
-				username
-				,password
-			)VALUES('admin', MD5('admin'))
-		";
-
-		$database->prepare($query)->execute();
-		
-		if($_SESSION['idusers'] == 0){
-			$query = "
-				SELECT 
-					IFNULL(column_name, '') INTO @colName
-				FROM information_schema.columns 
-				WHERE table_name = 'users'
-				AND column_name = 'usermail';
-
-				IF @colName = '' THEN 
-					ALTER TABLE users ADD usermail VARCHAR(50) NOT NULL
-				END IF;	
-			";
-
-			$database->prepare($query)->execute();	
-
-			$query = "
-				SELECT 
-					IFNULL(usermail, '') INTO @adminMail
-				FROM users
-				WHERE idusers = 0;
-				
-				IF @adminMail = '' THEN
-					BEGIN
-					UPDATE users SET
-						usermail = 'admin@admin.icm'
-					WHERE idusers = 0
-					END
-				END IF
-			";
-
-			$database->prepare($query)->execute();
-
-			$query = "
-				CREATE TABLE  IF NOT EXISTS  `$db_name`.`ReceivedEmails` (
-					`idmail` INT NOT NULL AUTO_INCREMENT,
-					`iduser` INT NOT NULL,
-					`fromID` INT NOT NULL,
-					`msg` VARCHAR(255) NULL,
-					PRIMARY KEY (`idmail`)
-				);
-			";
-
-			$database->prepare($query)->execute();
-
-
-			$query = "
-				SELECT 
-					IFNULL(column_name, '') INTO @colName
-				FROM information_schema.columns 
-				WHERE table_name = 'ReceivedEmails'
-				AND column_name = 'subject';
-
-				IF @colName = '' THEN 
-					ALTER TABLE ReceivedEmails ADD subject VARCHAR(255) NULL;
-				END IF;			
-			";		
-
-			$database->prepare($query)->execute();
-
-			$query = "
-				SELECT 
-					IFNULL(column_name, '') INTO @colName
-				FROM information_schema.columns 
-				WHERE table_name = 'users'
-				AND column_name = 'name';
-
-
-				IF @colName = '' THEN 
-					ALTER TABLE `$db_name`.`users` ADD name VARCHAR(45) NULL;
-				END IF;
-
-				SELECT 
-					IFNULL(column_name, '') INTO @colName
-				FROM information_schema.columns 
-				WHERE table_name = 'users'
-				AND column_name = 'lastname';
-
-				IF @colName = '' THEN 
-					ALTER TABLE `$db_name`.`users` ADD lastname VARCHAR(45) NULL;
-				END IF;
-
-
-			";		
-
-			$database->prepare($query)->execute();			
-
-
-			echo "<h1>Database changes ready</h1>";		
-		
-		}else{
-			echo "You have no access";
-		}
-	
-
-?>
